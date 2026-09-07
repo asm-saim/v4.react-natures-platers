574fc158e5c027257d8f91b8a489ae0abd4bb13b
asm-saim <saim777m@gmail.com>
2026-09-07 20:01:05 +0600

    tailwind configured and nav completed

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,5 +1,35 @@
+import { IoIosSearch } from "react-icons/io";
+import { IoCartOutline } from "react-icons/io5";
+import NavLogo from "../assets/assets/nav-logo.png";
 const Nav = () => {
-  return <div></div>;
+  return (
+    <div className="bg-amber-200  max-w-6xl mx-auto  flex justify-between items-center p-3">
+      <div>
+        <img className="h-10 w-32" src={NavLogo} alt="" />
+      </div>
+
+      <ul className="flex gap-5">
+        <li>
+          <a href="">Home</a>
+        </li>
+        <li>
+          <a href="">About</a>
+        </li>
+        <li>
+          <a href="">Collections</a>
+        </li>
+        <li>
+          <a href="">Contact</a>
+        </li>
+      </ul>
+      <div className="flex items-center gap-3">
+        <IoIosSearch />
+        <IoCartOutline />
+        <button>Login</button>
+        <button>SignUp</button>
+      </div>
+    </div>
+  );
 };
 
 export default Nav;
